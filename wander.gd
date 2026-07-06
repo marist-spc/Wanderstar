0cc3b4df81270f3d12a8057fa85a00e30dab9bf4
ERoboBloodfist <elewiscelis@gmail.com>
2026-07-06 14:15:31 -0400

    Level1 return

---
+++ wander.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
+signal recall
+signal star_move
+signal star_attack
+
+func _process(delta):
+	var mouse_position = get_global_mouse_position()
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_pressed("star_return"):
+		#move star to player
+		recall.emit(position)
+	elif Input.is_action_pressed("move_star"):
+		#move star to mouse
+		star_move.emit(get_global_mouse_position())
+	elif Input.is_action_just_pressed("shoot_star"):
+		star_attack.emit()
+
+	move_and_slide()
+	
