1a880804d994382868892458547f260c11decaa1
ERoboBloodfist <elewiscelis@gmail.com>
2026-07-06 14:38:48 -0400

    Adjustments

--- star.gd
+++ star.gd
@@ -15,7 +15,9 @@ func _physics_process(delta):
 	#move the star to wherever its supposed to be going
 	if target_position != position:
 		position = position.move_toward(target_position, 1000*delta)
-	
+	# difference of target_pos and pos is the vector pointing to where star should go
+	# this can be the velocity
+	# then use move_and_slide to actually move star with collision-0detection
 func _process(delta):
 	look_at(get_global_mouse_position())
 
