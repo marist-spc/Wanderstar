e712ba5e9ad2638de34912226990a4b02d99cd8b
Zer0Brian <matthew.schick.2027@gmail.com>
2026-07-07 10:11:43 -0400

    health and animations

--- monster_1.gd
+++ monster_1.gd
@@ -31,5 +31,5 @@ func _physics_process(delta: float): #Basic Goomba Code
 func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	health -= 1
 	if health == 0:
-		hide()
+		queue_free()
 	
